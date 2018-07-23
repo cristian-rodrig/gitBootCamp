fd6a516bc5f35c957db5775c3775244d32b21b3e
cristian-rodrig <41455900+cristian-rodrig@users.noreply.github.com>
2018-07-23 14:38:00 -0300

    Update ShoppingCartService.java
    Include javadoc

--- src/main/java/com/bootcamp/topic3/service/ShoppingCartService.java
+++ src/main/java/com/bootcamp/topic3/service/ShoppingCartService.java
@@ -5,10 +5,35 @@ import com.bootcamp.topic3.cart.ShoppingCart;
 
 public interface ShoppingCartService {
 	
+	/**
+	 * 
+	 * @param CartItem. Item that will be added to cart.
+	 */
 	public void addCartItem(CartItem item);
+	
+	/**
+	 * 
+	 * @param CartItem. Item that will be delete from the cart.
+	 */
 	public boolean deleteCartItem (CartItem item);
+	
+	
+	/**
+	 * 
+	 * @param int id. Item that will be get it from the cart.
+	 */
 	public CartItem getCartItem (int id);
+	
+	/**
+	 * 
+	 * @param create a shopping cart.
+	 */
 	public ShoppingCart createShoppingCart ();
+	
+	/**
+	 * 
+	 * @param clear the cart.
+	 */
 	public void clearCart();
 
 }
