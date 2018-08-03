aae06ab0bff94439fc1afc61b46b919e6f3d32ab
cristian-rodrig <41455900+cristian-rodrig@users.noreply.github.com>
2018-08-02 21:54:59 -0300

    Update Student_Realized_CourseDAO.java
    Rename class remove underscore.

--- src/main/java/com/mongoDBS/dao/Student_Realized_CourseDAO.java
+++ src/main/java/com/mongoDBS/dao/Student_Realized_CourseDAO.java
@@ -10,14 +10,14 @@ import com.mongoDBS.entities.Course;
 import com.mongoDBS.entities.Student_Realized_Course;
 import com.mongodb.MongoClient;
 	
-public class Student_Realized_CourseDAO extends BasicDAO<Student_Realized_Course, String>{
+public class StudentRealizedCourseDAO extends BasicDAO<Student_Realized_Course, String>{
 
-		public Student_Realized_CourseDAO(Morphia morphia, MongoClient mongo, String dbName) {
+		public StudentRealizedCourseDAO(Morphia morphia, MongoClient mongo, String dbName) {
 			super(mongo, morphia, dbName);
 		}
 		
 		public QueryResults<Student_Realized_Course> findNotesGraterThan(Datastore datastore, Course course, int final_note) {
-			Query<Student_Realized_Course> query = datastore.createQuery(Student_Realized_Course.class);
+			Query<StudentRealizedCourse> query = datastore.createQuery(StudentRealizedCourse.class);
 			query.and(query.criteria("course").equal(course))
 			.and(query.criteria("final_note").greaterThanOrEq(final_note));
 			return find(query);
